89d472a279e4e3aea703a611cee465610b7cd2fa
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-01-15 22:03:14 -0800

    more debugging

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var session = require('express-session')
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(ejsLayouts);
 app.use(session({
